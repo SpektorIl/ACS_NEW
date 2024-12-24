020d71108d48767d7fa2461b2fa93b176b38e56d
Ilya Spektor <ilyaspektor@mail.ru>
2024-12-24 20:07:14 +0400

    lab4#2

--- src/main/java/com/example/demo/services/BookService.java
+++ src/main/java/com/example/demo/services/BookService.java
@@ -2,6 +2,8 @@ package com.example.demo.services;
 
 import com.example.demo.models.Book;
 import com.example.demo.models.repositories.BookRepository;
+import com.example.demo.services.jms.ChangeLogMessage;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,10 +13,11 @@ import java.util.Optional;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final JmsTemplate jmsTemplate;
 
-
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, JmsTemplate jmsTemplate) {
         this.bookRepository = bookRepository;
+        this.jmsTemplate = jmsTemplate;
     }
 
     public List<Book> findAll(){
@@ -26,11 +29,19 @@ public class BookService {
     }
 
     public Book save(Book book){
+        Book savedBook = bookRepository.save(book);
+        sendChangeLog("CREATE_OR_UPDATE", "Book", savedBook.getId(), "Book saved or updated");
         return bookRepository.save(book);
     }
 
     public void deleteById(Long id){
         bookRepository.deleteById(id);
+        sendChangeLog("DELETE", "Book", id, "Book deleted");
+    }
+
+    private void sendChangeLog(String changeType, String entityClass, Long entityId, String details) {
+        ChangeLogMessage message = new ChangeLogMessage(changeType, entityClass, entityId, details);
+        jmsTemplate.convertAndSend("jms/ChangeLogQueue", message);
     }
 
 }
